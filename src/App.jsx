cb8043d351e2a20b9d97ab85663d6426211a8e0d
mayedakonika <mayedakonika@gmail.com>
2024-01-14 17:15:32 +0600

    html file to react component implement added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
